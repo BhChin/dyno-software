8ed35d798f3c439f38d9a6e686f8ddad29b581c8
BhChin <brandonhchin@gmail.com>
2026-06-14 19:15:13 -0700

    Dyno reader

--- teensy-simulator.cpp
+++ teensy-simulator.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <sstream>
 #include <thread>
 #include <chrono>
 
@@ -13,7 +14,30 @@
 // data flow:
 // sensors -> teensy -> dyno software
 
+//dyno
+void reader(const std::string& line) {
+    double rpm;
+    double torque;
+    double temp;
+    double horsepower;
+    char comma1, comma2, comma3;
 
+    std::stringstream ss(line);
+
+    ss >> rpm >> comma1
+       >> torque >> comma2
+       >> temp >> comma3
+       >> horsepower;
+
+    std::cout << "Parsed:\n";
+    std::cout << "RPM: " << rpm << '\n';
+    std::cout << "Torque: " << torque << '\n';
+    std::cout << "Temp: " << temp << '\n';
+    std::cout << "Horsepower: " << horsepower << '\n';
+    std::cout << "----------------\n";
+}
+
+// teensy
 int main() {
     double rpm = 0; 
     double torque = 0;
@@ -26,14 +50,22 @@ int main() {
 
         double horsepower = torque * rpm / 5252.0;
 
-        std::cout << rpm << "," << torque << "," << temp << "," << horsepower << '\n';
+        // simulates the teensy sending the output
+        std::stringstream teensyOutput;
+        teensyOutput
+            << rpm << ","
+            << torque << ","
+            << temp << ","
+            << horsepower;
+
+        std::string line = teensyOutput.str();
+
+        // simulates the dyno software recieving the teensy output
+        reader(line);
 
         std::this_thread::sleep_for(std::chrono::milliseconds(500));
     }
 
     return 0;
 }
-
-
-
 
